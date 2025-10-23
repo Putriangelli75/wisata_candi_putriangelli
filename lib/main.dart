e81dde5bcf49407756bafad8b8fbe36fbf2e4b11
Putriangelli75 <putriangelli_2428240091@mhs.mdp.ac.id>
2025-10-23 21:25:56 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,25 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Wisata Candi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: DetailScreen(),
+      home: DetailScreen(candi: candiList[0]),
     );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,12 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/models/candi.dart';
+import 'package:wisata_candi/widgets/detail_gallery.dart';
+import 'package:wisata_candi/widgets/detail_header.dart';
+import 'package:wisata_candi/widgets/detail_info.dart';
 
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
+class DetailScreen extends StatefulWidget {
+  final Candi candi;
+  const DetailScreen({super.key, required this.candi});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  bool isFavorite = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isFavorite = widget.candi.isFavorite;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body:
-    Column(
-      children: [],),);
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            DetailHeader(
+              imageUrl: widget.candi.imageUrls.first,
+              onBackPressed: () => Navigator.pop(context),
+            ),
+            DetailInfo(
+              candi: widget.candi,
+              isFavorite: isFavorite,
+              onToggleFavorite: toggleFavorite,
+            ),
+            DetailGallery(imageUrls: widget.candi.imageUrls),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void toggleFavorite() {
+    setState(() {
+      isFavorite = !isFavorite;
+      widget.candi.isFavorite = isFavorite;
+    });
   }
 }
