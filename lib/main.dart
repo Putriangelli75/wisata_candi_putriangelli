65181db945f1132374f72d46e4df2f5404187aaf
Putriangelli75 <putriangelli_2428240091@mhs.mdp.ac.id>
2025-11-06 07:28:54 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
       ),
       home: DetailScreen(candi: candiList[0]),
+      // home: ProfileScreen()
     );
   }
 }
